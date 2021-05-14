8fd17af8fb0c5005c9409ce2c1c15bc19876dbed
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-14 16:04:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+mapToNegativize(sourceArray);
+mapToNoChange(sourceArray);
+mapToDouble(sourceArray);
+mapToSquare(sourceArray);
+
+
+reduceToTotal(sourceArray, startingPoint);
+reduceToAllTrue(sourceArray);
+reduceToAnyTrue(sourceArray);
