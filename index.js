10a04a9b913c008eb8941107dbefd7c676daa1c7
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-14 16:28:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-// Your code here
+const sourceArray= [1, 2, 3, -9];
+const array= [];
+
+function mapToNegativize(array) {
+    let result = array.map(function(number){
+        return -number ;
+})
+    return result;
+};
+
+function mapToDouble(array) {
+    let result = array.map(function(number){
+        return number * 2 ;
+})
+    return result;
+};
+
+function mapToSquare(array) {
+    let result = array.map(function(number){
+        return number * number;
+})
+    return result;
+};
+
+function mapToNoChange(array) {
+    let result = array.map(function(number){
+        return number;
+})
+    return result;
+};
+
+mapToNegativize(sourceArray);
+mapToDouble(sourceArray);
+mapToSquare(sourceArray);
+mapToNoChange(sourceArray);
+
+
+function reduceToTotal(array, startingPoint = 0){
+    let result = array.reduce(function(accumulator, number) {
+        return accumulator + number;
+      },startingPoint)
+      return result;
+}
+
+const reduceToAllTrue = (arr) => {
+    return arr.reduce(function(accumulator, currentValue) {
+     if(currentValue){
+       return true;
+     } else {
+       return false;
+     }
+  })
+  }
+
+  function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++ ) {
+      if (array[i]){
+        return true;
+      }}
+    return false;
+};
+
+
+
+reduceToTotal(sourceArray);
+reduceToTotal(sourceArray, 10);
+reduceToAllTrue(sourceArray);
+reduceToAnyTrue(sourceArray);
