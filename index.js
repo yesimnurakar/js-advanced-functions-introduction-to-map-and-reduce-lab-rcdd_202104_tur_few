3c6c8bf58e215e3c1c34823674e5ae8a09a255b7
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-14 16:19:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
-// Your code here
-const sourceArray=[1,2,3,-9];
-function mapToNegativize() {
-  const new = sourceArray.map(x => x * -1);
-  return new}
-console.log(mapToNegativize);
+const sourceArray = [1, 2, 3, -9];
 
+function mapToNegativize(array) {
+    let result = array.map(function(number){
+        return -number ;
+})   
+    return result;
+};
 
+function mapToDouble(array) {
+    let result = array.map(function(number){
+        return number * 2 ;
+})   
+    return result;
+};
 
-mapToNoChange(sourceArray);
+function mapToSquare(array) {
+    let result = array.map(function(number){
+        return number * number;
+})   
+    return result;
+};
+
+function mapToNoChange(array) {
+    let result = array.map(function(number){
+        return number;
+})   
+    return result;
+};
+
+mapToNegativize(sourceArray);
 mapToDouble(sourceArray);
 mapToSquare(sourceArray);
+mapToNoChange(sourceArray);
 
 
-reduceToTotal(sourceArray, startingPoint);
+function reduceToTotal(array, startingPoint = 0){
+    let result = array.reduce(function(accumulator, number) {
+        return accumulator + number
+      },startingPoint)
+
+      return result;
+}
+
+const reduceToAllTrue = (arr) => {
+    return arr.reduce(function(accumulator, currentValue) {
+     if(currentValue){
+       return true
+     } else {
+       return false
+     }
+  })
+  }
+  console.log(reduceToAllTrue(value))
+
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < array.length; i++ ) {
+      if (array[i]){
+        return true;
+      }}
+    return false
+};
+
+
+
+reduceToTotal(sourceArray);
+reduceToTotal(sourceArray, 85);
 reduceToAllTrue(sourceArray);
 reduceToAnyTrue(sourceArray);
